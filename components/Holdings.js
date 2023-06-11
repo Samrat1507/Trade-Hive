4781a7921b3275b870da3332a0cf5126b5b504ee
Jayanti2919 <jayanti2919@gmail.com>
2023-06-12 01:20:52 +0530

    added holdings to buy-sell

--- components/Holdings.js
+++ components/Holdings.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useRouter } from "next/router";
 
 const Holdings = ({ holdings }) => {
+  const router = useRouter();
   return (
-    <div>
-        {holdings}
+    <div className="flex flex-col gap-3">
+      {Object.entries(holdings).map(([key, value]) => (
+        <div className="flex justify-between">
+          <div key={key} className="text-accent flex gap-16 items-center">
+            <span>{key} </span>
+            <span>{value}</span>
+          </div>
+          <button
+            className="button-bg text-primary px-3 py-1 rounded-lg"
+            onClick={(e) => {
+              e.preventDefault();
+              router.push("/protected/Payment");
+            }}
+          >
+            Sell
+          </button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Holdings
+export default Holdings;
--- src/pages/protected/buy-sell.js
+++ src/pages/protected/buy-sell.js
@@ -7,32 +7,33 @@ import Holdings from '../../../components/Holdings'
 
 const BuySell = () => {
   const snap = useSnapshot(state)
+  console.log(state)
 
   return (
     <div className='primary-bg h-fit min-h-screen '>
       <ProtectedNav/>
       <ProtectedSidebar active="Buy and Sell" />
 
-      <div className={`${state.sidebar ? 'md:pl-[30vw]' : 'md:pl-[5vw]'}`}>
+      <div className={`${state.sidebar ? 'md:pl-[32vw] xl:pl-[30vw]' : 'md:pl-[5vw]'} pl-5 mt-10`}>
         <h1 className='text-5xl text-secondary font-semibold '>
           Buy and Sell
         </h1>
 
-        <div className='mt-20'>
-          <div className='h-fit widgets w-fit flex flex-col gap-5 pr-10 pl-4 rounded-xl py-5'>
+        <div className='mt-20 pr-5'>
+          <div className='h-fit widgets w-fit md:min-w-[35vw] min-w-full flex flex-col gap-5 pr-10 pl-4 rounded-xl py-5'>
             <h2 className='text-2xl text-accent font-medium'>Current Holdings</h2>
             <div>
               <div className='flex gap-10 text-accent'>
                 <span>Company</span>
                 <span>Quantity</span>
               </div>
-              <div className='w-full h-[1px] bg-secondary'>
+              <div className='w-full h-[1px] bg-secondary mt-2'>
               </div>
 
-              <div>
+              <div className='mt-4'>
                 {
                   state.user.holdings ? (
-                    <Holdings />
+                    <Holdings holdings={state.user.holdings}/>
                   ) : (<span className='text-accent'>No Holdings were Found</span>)
                 }
               </div>
